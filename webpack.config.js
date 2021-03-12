b15747df1e56e881b6c905d81d881156e0bf9549
Miguel Ricaño <mricanho@gmail.com>
2021-03-12 15:31:54 -0600

    Fix eslint error

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,6 @@ module.exports = {
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
-    publicPath: "/Restaurant-page/",
+    publicPath: '/Restaurant-page/',
   },
 };
